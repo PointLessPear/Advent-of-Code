7677304354a93103f5772e03a04425910d938b83
PointLessPear <56558102+PointLessPear@users.noreply.github.com>
2019-12-03 21:19:26 +0000

    Add files via upload

---
+++ Advent of code 1.py
@@ -0,0 +1,23 @@
+
+
+import os
+
+file = open('AOC1.txt', 'r').read()
+MassItem = file.split('\n')
+
+
+
+def getfuel(mass):
+
+        return int(mass / 3) - 2
+
+
+totalfuel = 0
+for mass in MassItem:
+     mass = int(mass)
+     calcdmass = getfuel(mass)
+
+     totalfuel = totalfuel + calcdmass
+
+
+print(totalfuel)
